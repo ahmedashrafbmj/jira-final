953452f3fb335145ac9bc603791a13d75662aae1
ahmedashrafbmj <ahmedzain7557@gmail.com>
2023-12-27 19:57:08 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 //import Dashboard from "./components/dashboard/index.js";
-import RouterComponenet from "./Routes/index.js";
+import RouterComponenet from "./Routes/index.js"
 
 export default function App() {
   return (
